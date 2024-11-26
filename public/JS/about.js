02ff6d44dc71c191889b2ae770d51fa6082c97e2
Ntad18 <60619452+Ntad18@users.noreply.github.com>
2024-11-26 00:09:59 -0500

    big brain

---
+++ public/JS/about.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const isLoggedIn = localStorage.getItem('loggedIn');
+    const hasUserID = localStorage.getItem('userId')
+    const loginStatus = document.getElementById('loginStatus');
+    const signInDiv = document.getElementById('signInDiv');
+
+    //The two buttons to the right of the nav bar. sign in and create acc
+    const btn1 = document.getElementById('signIn');
+    const btn2 = document.getElementById('createAcc');
+
+    //current userID
+    const userId = localStorage.getItem('userId');
+
+    //test comment
+    if (isLoggedIn) {
+        //once logged in, we will change the contents and href of btn 1 (sign in) to lead to account
+        btn1.querySelector('a').innerText = 'Account';
+        btn1.querySelector('a').href = '../HTML/account.html';
+
+        //once logged in, we will change the contents and href of btn2 (create account) to log out and index.html
+        btn2.querySelector('a').innerText = 'Logout';
+
+        // we want to prevent it from going right away, (only want it onclick)
+        btn2.querySelector('a').href = '#'; 
+
+        //onclick, logout and go to home page
+        btn2.onclick = function(){
+            //clear login status
+            localStorage.removeItem('loggedIn');
+            localStorage.removeItem('userId');
+            
+            //bring back to home page
+            window.location.href = '../HTML/index.html';
+
+        }
+
+    } else {
+        loginStatus.style.display = '';
+    }
+});
